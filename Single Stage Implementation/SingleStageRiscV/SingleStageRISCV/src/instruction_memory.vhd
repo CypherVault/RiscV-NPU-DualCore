687764b17150f6b0e28acadc58676ec4a8a50a37
chrisMM <Mystic0w1@outlook.com>
2024-10-22 00:42:40 -0400

    added registers and the immediategen vhd files based off book for single cycle.

--- Single Stage Implementation/SingleStageRiscV/SingleStageRISCV/src/instruction_memory.vhd
+++ Single Stage Implementation/SingleStageRiscV/SingleStageRISCV/src/instruction_memory.vhd
@@ -4,14 +4,13 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity instruction_memory is
     Port ( 
-        
         pc_address : in STD_LOGIC_VECTOR(15 downto 0);
         instruction : out STD_LOGIC_VECTOR(31 downto 0)
     );
 end instruction_memory;
 
 architecture Behavioral of instruction_memory is
-    type memory_array is array (0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
+    type memory_array is array (0 to 31) of STD_LOGIC_VECTOR(31 downto 0);
     constant mem : memory_array := (
         0 => x"00000000",
         1 => x"00100013",
