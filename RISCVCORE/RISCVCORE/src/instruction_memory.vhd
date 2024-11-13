01d1a3c7b54519dcfd6b513e6a0686654becee92
chrisMM <Mystic0w1@outlook.com>
2024-11-12 20:10:00 -0500

    exapnded the test program to include load and store instructiosn , data hazards, and some control haazrds. we seem to have some hardwaree issues with dealing with BEQ,BNE,JAL potentially. but tthe final register states are close. just need to ensure the jumps and stuff ACTUALLY happen. this processor is getting really close, time to start cleaning up the code adding more inline commments, explanantions, making a real way for the ARM to load instrutcions and extarct memory from reg and data mem based on addresses. we should move to testing long compiled exmaples, cleaning the github registry preparing for production, and FPGA implenentation and routing.

--- RISCVCORE/RISCVCORE/src/instruction_memory.vhd
+++ RISCVCORE/RISCVCORE/src/instruction_memory.vhd
@@ -13,10 +13,11 @@ architecture Behavioral of instruction_memory is
     type memory_array is array (0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
     constant mem : memory_array := (
         0 => x"00000000",
-       1 => x"02A00093",
-        2 => x"00102623",
-        3 => x"00C02103",
-        4 => x"00A10193",                            --load and add test program
+        1 => x"00A00093",  -- addi x1, x0, 10
+    2 => x"01400113",  -- addi x2, x0, 20
+    3 => x"00206463",  -- blt x1, x2, label
+    4 => x"01E00193",  -- addi x3, x0, 30
+    5 => x"02800213",  -- addi x4, x0, 40
 		others => x"00000000"
     );
 
